0854e784b68feca178da91499ee05c366036f38f
Sam.S <s.anonymity@gmail.com>
2017-08-26 01:03:09 +1000

    Progress commit, adding images, working on home page layout

---
+++ src/containers/Videospage/index.js
@@ -0,0 +1,55 @@
+'use strict';
+
+import './styles.scss';
+
+import EventManagerMixin from 'mixins/EventManagerMixin';
+import FadeTransitionMixin from 'mixins/FadeTransitionMixin';
+
+import {
+  WINDOW_RESIZE
+} from 'config/messages';
+
+import ExampleComponent from 'components/Example';
+import HeaderComponent from 'components/Header';
+import SidebarComponent from 'components/Sidebar';
+import CarouselComponent from 'components/Carousel';
+
+export default Vue.extend({
+
+  mixins: [ EventManagerMixin, FadeTransitionMixin ],
+
+  template: require( './template.html' ),
+
+  emitterEvents: [{
+    message: WINDOW_RESIZE,
+    method: 'onWindowResize'
+  }],
+
+  data() {
+
+    return {
+      _hidden: null
+    };
+  },
+
+  created() {
+
+  },
+
+  methods: {
+
+    onWindowResize( {width, height} ) {
+      /*eslint-disable */
+      console.log( `Window resize from application with debounce -> width: ${width}px || height: ${ height }` );
+      /*eslint-enable */
+    }
+
+  },
+
+  components: {
+    'example-component': ExampleComponent,
+    'pageheader': HeaderComponent,
+    'sidebar': SidebarComponent,
+    'carousel': CarouselComponent
+  }
+});
